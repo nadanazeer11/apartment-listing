c475751c91b3854fb095ce8b86ee7a34911db025
nadanzeer11 <nadanazeer11@gmail.com>
2025-04-29 11:59:04 +0300

    feat: connect to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const MONGODB_URI = process.env.MONGODB_URI || '';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(MONGODB_URI),
+  ],
 })
 export class AppModule {}
